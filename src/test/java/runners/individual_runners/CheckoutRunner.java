7ea47e015bec7fd369e0a2b68790a325559ef840
unknown <orlando.avilag@hotmail.com>
2023-04-05 16:42:28 -0600

    Global runner added.

---
+++ src/test/java/runners/individual_runners/CheckoutRunner.java
@@ -0,0 +1,19 @@
+package runners.individual_runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/java/project/features/checkout/checkout.feature",
+        glue = {"project/steps_definition"},
+        plugin = {
+                "pretty", "html:src/main/java/project/reports/html/report.html",
+                "json:src/main/java/project/reports/json/report.json"
+        }
+)
+
+public class CheckoutRunner
+{
+}
