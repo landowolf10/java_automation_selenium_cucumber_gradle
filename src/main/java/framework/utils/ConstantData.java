cbcfc7bfd8186ab680112eb1b3672303d4f0e224
Orlando Avila <orlando.avilag@hotmail.com>
2023-08-13 15:05:20 -0600

    CircleCI integration

--- src/main/java/framework/utils/ConstantData.java
+++ src/main/java/framework/utils/ConstantData.java
@@ -2,6 +2,6 @@ package framework.utils;
 
 public class ConstantData {
     public static String chromeDriver = "webdriver.chrome.driver";
-    public static String driverPath = "/home/circleci/project/src/main/java/framework/drivers/chromedriver";
+    public static String driverPath = "/src/main/java/framework/drivers/chromedriver";
     public static String URL = "https://www.saucedemo.com/";
 }
